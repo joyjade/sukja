0797100bd7d446005731a6b1247cb94293e15fd2
JJ <jadeandpearls@gmail.com>
2025-07-29 15:16:53 -0600

    image srcset adjustments;

--- site/snippets/painting-srcset.php
+++ site/snippets/painting-srcset.php
@@ -1,14 +1,21 @@
 <figure>
-  <img
+  <?php if($max == 'full') : ?>
+    <img
+        loading="eager"
+        alt="<?= $photo->alt() ?>"
+        src="<?= $photo->resize(400)->url() ?>"
+        srcset="<?= $photo->srcset([400, 600, 900, 1200, 1800]) ?>"
+        width="<?= $photo->resize(1800)->width() ?>"
+        height="<?= $photo->resize(1800)->height() ?>"
+    >
+  <?php else : ?>
+    <img
       loading="lazy"
       alt="<?= $photo->alt() ?>"
       src="<?= $photo->resize(400)->url() ?>"
-      <?php if($max == 'full') : ?>
-        srcset="<?= $photo->srcset([400, 600, 900, 1200, 1800]) ?>"
-      <?php else : ?>
-        srcset="<?= $photo->srcset([400, 600, 900]) ?>"
-      <?php endif ?>
-      width="<?= $photo->resize(1800)->width() ?>"
-      height="<?= $photo->resize(1800)->height() ?>"
-  >
+      srcset="<?= $photo->srcset([400, 600, 900]) ?>"
+      width="<?= $photo->resize(900)->width() ?>"
+      height="<?= $photo->resize(900)->height() ?>"
+    >
+  <?php endif ?>
 </figure>
